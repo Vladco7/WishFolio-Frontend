924138db3dae0c6c2eff9030ed47781841ec21cf
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2025-01-19 20:39:26 +0300

    added TypographyComponent

---
+++ wish-folio-frontend/src/components/UI/TypographyComponent/TypographyComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { TypographyVariantToStylesMap, type TypographyComponentProps } from './types';
+
+const props = defineProps<TypographyComponentProps>()
+
+const variantStyles = TypographyVariantToStylesMap[props.variant]
+
+</script>
+
+<template>
+    <div :style="{
+        fontSize: variantStyles.fontSize,
+        fontWeight: variantStyles.fontWeight,
+        lineHeight: variantStyles.lineHeight
+    }">
+        <slot />
+    </div>
+</template>
