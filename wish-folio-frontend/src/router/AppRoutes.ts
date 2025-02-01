8e6f7f1d09d6e8dff63e91cd9f85e07e030e56e2
VL <leonencovlad@gmail.com>
2025-02-01 15:38:03 +0300

    added register route to AppRoutes

--- wish-folio-frontend/src/router/AppRoutes.ts
+++ wish-folio-frontend/src/router/AppRoutes.ts
@@ -1,9 +1,11 @@
 export enum AppRoutesNames {
     HOME = "home",
     LOGIN = "login",
+    REGISTER = "register",
 }
 
 export const AppRoutesPaths = {
     [AppRoutesNames.HOME]: "/",
     [AppRoutesNames.LOGIN]: "/login",
+    [AppRoutesNames.REGISTER]: "/register",
 }
