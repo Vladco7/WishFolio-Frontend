df88190dcb59b7b2dff684d60a2fdfd5ae55863b
VL <leonencovlad@gmail.com>
2025-02-01 18:10:16 +0300

    added AuthorizationContainer component for login and register pages

---
+++ wish-folio-frontend/src/components/UI/AuthorizationContainer/AuthorizationContainer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  width: 100%;
+}
+</style>
