e885b31b3df021503f515ec5bbb4a9b86b349c91
VL <leonencovlad@gmail.com>
2025-02-01 18:10:43 +0300

    added AuthorizationContainer and link to register page on login page

--- wish-folio-frontend/src/components/LoginPage/LoginPage.vue
+++ wish-folio-frontend/src/components/LoginPage/LoginPage.vue
@@ -1,69 +1,74 @@
 <script setup lang="ts">
 import { Form, type FormSubmitEvent } from '@primevue/forms'
 import { Button, Card, InputText, Message, Password } from 'primevue'
-import FlexComponent from '../UI/FlexComponent/FlexComponent.vue';
-import TypographyComponent from '../UI/TypographyComponent/TypographyComponent.vue';
-import {TypographyVariant} from '../UI/TypographyComponent/types';
-import { login } from '@/api/authorization/login';
-import tokenService from '@/services/tokenService';
-import router from '@/router';
-import { AppRoutesNames } from '@/router/AppRoutes';
-import { ref } from 'vue';
-import { zodResolver } from '@primevue/forms/resolvers/zod';
-import { z } from 'zod';
+import FlexComponent from '../UI/FlexComponent/FlexComponent.vue'
+import TypographyComponent from '../UI/TypographyComponent/TypographyComponent.vue'
+import { TypographyVariant } from '../UI/TypographyComponent/types'
+import { login } from '@/api/authorization/login'
+import tokenService from '@/services/tokenService'
+import router from '@/router'
+import { AppRoutesNames } from '@/router/AppRoutes'
+import { ref } from 'vue'
+import { zodResolver } from '@primevue/forms/resolvers/zod'
+import { z } from 'zod'
+import AuthorizationContainer from '../UI/AuthorizationContainer/AuthorizationContainer.vue'
 
 const initialValues = ref({
   email: '',
-  password: ''
-});
+  password: '',
+})
 
-const resolver = zodResolver(z.object({
-  email: z.string().email({message: 'Неверная электронная почта'}),
-  password: z.string().min(1, {message: 'Пароль должен быть не менее 1 символа'})
-}))
+const resolver = zodResolver(
+  z.object({
+    email: z.string().email({ message: 'Неверная электронная почта' }),
+    password: z.string().min(1, { message: 'Пароль должен быть не менее 1 символа' }),
+  }),
+)
 
 const onSubmit = async (event: FormSubmitEvent) => {
-  console.log(event);
-  if(!event.valid) return
-    const response = await login({email: event.states.email.value, password: event.states.password.value})
-    if(!response) return
-    tokenService.setToken(response.value)
-    router.push({ name: AppRoutesNames.HOME })
+  console.log(event)
+  if (!event.valid) return
+  const response = await login({
+    email: event.states.email.value,
+    password: event.states.password.value,
+  })
+  if (!response) return
+  tokenService.setToken(response.value)
+  router.push({ name: AppRoutesNames.HOME })
 }
-
 </script>
 
 <template>
-  <div class="container">
+  <AuthorizationContainer>
     <Card>
-        <template #content>
-        <Form v-slot="$form" @submit="onSubmit" :initial-values="initialValues" :resolver="resolver" >
-            <FlexComponent :direction="'column'" :gap="'16px'">
-                <FlexComponent :align="'center'" :justify="'center'" >
-                <TypographyComponent :variant="TypographyVariant.h2">Вход</TypographyComponent>
+      <template #content>
+        <Form
+          v-slot="$form"
+          @submit="onSubmit"
+          :initial-values="initialValues"
+          :resolver="resolver"
+        >
+          <FlexComponent :direction="'column'" :gap="'16px'">
+            <FlexComponent :align="'center'" :justify="'center'">
+              <TypographyComponent :variant="TypographyVariant.h2">Вход</TypographyComponent>
             </FlexComponent>
-                <InputText name="email" type="text" placeholder="E-mail" />
-                <Message v-if="$form.email?.invalid" severity="error" size="small" variant="simple">
-                  {{ $form.email.error.message }}
-                </Message>
-                <Password name="password" :feedback="false" placeholder="Пароль" toggleMask />
-                <Message v-if="$form.password?.invalid" severity="error" size="small" variant="simple">
-                  {{ $form.password.error.message }}
-                </Message>
-                <Button type="submit">Войти</Button>
-            </FlexComponent>
+            <InputText name="email" type="text" placeholder="E-mail" />
+            <Message v-if="$form.email?.invalid" severity="error" size="small" variant="simple">
+              {{ $form.email.error.message }}
+            </Message>
+            <Password name="password" :feedback="false" placeholder="Пароль" toggleMask />
+            <Message v-if="$form.password?.invalid" severity="error" size="small" variant="simple">
+              {{ $form.password.error.message }}
+            </Message>
+            <Button type="submit">Войти</Button>
+            <Button as="router-link" :to="{ name: AppRoutesNames.REGISTER }" severity="secondary"
+              >Регистрация</Button
+            >
+          </FlexComponent>
         </Form>
-    </template>
+      </template>
     </Card>
-  </div>
+  </AuthorizationContainer>
 </template>
 
-<style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-}
-</style>
+<style scoped></style>
