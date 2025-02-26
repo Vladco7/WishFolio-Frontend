bd0c8951de2beb3f36c55b90085ddfe871b77f29
VL <leonencovlad@gmail.com>
2025-02-26 23:15:37 +0300

    get user on token change

--- wish-folio-frontend/src/App.vue
+++ wish-folio-frontend/src/App.vue
@@ -1,5 +1,28 @@
 <script setup lang="ts">
+import { watch } from 'vue'
+import { getProfile } from './api/profile/getProfile'
 import AppError from './components/AppError/AppError.vue'
+import { useTokenStore } from './stores/token/token'
+import { useUserStore } from './stores/user/user'
+import { RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
+
+const { token } = storeToRefs(useTokenStore())
+const { setUser } = useUserStore()
+
+const getUser = async () => {
+  const response = await getProfile()
+  if (response) {
+    setUser(response)
+  }
+}
+
+if (token.value) getUser()
+
+watch(token, () => {
+  if (!token.value) return
+  getUser()
+})
 </script>
 
 <template>
