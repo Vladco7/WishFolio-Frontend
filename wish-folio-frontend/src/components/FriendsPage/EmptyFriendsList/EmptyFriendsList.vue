0814d74b255e25bbebaabe72239a37ced0882f90
VL <leonencovlad@gmail.com>
2025-03-09 15:50:59 +0300

    added EmptyFriendsList component

---
+++ wish-folio-frontend/src/components/FriendsPage/EmptyFriendsList/EmptyFriendsList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import FlexComponent from '@/components/UI/FlexComponent/FlexComponent.vue'
+import { TypographyVariant } from '@/components/UI/TypographyComponent/types'
+import TypographyComponent from '@/components/UI/TypographyComponent/TypographyComponent.vue'
+import { getIconClass, IconName } from '@/types/icon/icon'
+import { Button, Card } from 'primevue'
+
+const emit = defineEmits(['addFriend'])
+
+const onAddFriend = () => {
+  emit('addFriend')
+}
+</script>
+
+<template>
+  <Card>
+    <template #content>
+      <FlexComponent direction="column" gap="16px" :align="'center'">
+        <TypographyComponent :variant="TypographyVariant.h2"
+          >Список друзей пуст</TypographyComponent
+        >
+        <TypographyComponent :variant="TypographyVariant.body1"
+          >Добавьте друга, чтобы увидеть его в списке друзей</TypographyComponent
+        >
+        <Button
+          label="Добавить друга"
+          :icon="getIconClass(IconName.PLUS)"
+          icon-pos="right"
+          @click="onAddFriend"
+        />
+      </FlexComponent>
+    </template>
+  </Card>
+</template>
