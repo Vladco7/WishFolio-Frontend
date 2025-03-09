5f5a46f539ba88c7721bd06a2f3e87e6da86b027
VL <leonencovlad@gmail.com>
2025-03-09 16:11:42 +0300

    added GridComponent

---
+++ wish-folio-frontend/src/components/UI/GridComponent/GridComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { GridComponentProps } from './types'
+
+const props = defineProps<GridComponentProps>()
+</script>
+
+<template>
+  <div
+    class="grid-component"
+    :style="{
+      display: 'grid',
+      gridTemplateColumns: props.columns,
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
